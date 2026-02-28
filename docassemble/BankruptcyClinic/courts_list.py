e9fe2b6547bb3f073e3fc39982f24cab1e284a4a
Alex Clark <alex@clarkmanagementconsulting.com>
2026-02-28 12:17:09 -0500

    feat: Expand courts list to include all U.S. District Courts and territories

--- docassemble/BankruptcyClinic/courts_list.py
+++ docassemble/BankruptcyClinic/courts_list.py
@@ -1,3 +1,148 @@
 courts_list = [
-"District of Nebraska", 
-"District of South Dakota"]
+    # Alabama
+    "Middle District of Alabama",
+    "Northern District of Alabama",
+    "Southern District of Alabama",
+    # Alaska
+    "District of Alaska",
+    # Arizona
+    "District of Arizona",
+    # Arkansas
+    "Eastern District of Arkansas",
+    "Western District of Arkansas",
+    # California
+    "Central District of California",
+    "Eastern District of California",
+    "Northern District of California",
+    "Southern District of California",
+    # Colorado
+    "District of Colorado",
+    # Connecticut
+    "District of Connecticut",
+    # Delaware
+    "District of Delaware",
+    # District of Columbia
+    "District of Columbia",
+    # Florida
+    "Middle District of Florida",
+    "Northern District of Florida",
+    "Southern District of Florida",
+    # Georgia
+    "Middle District of Georgia",
+    "Northern District of Georgia",
+    "Southern District of Georgia",
+    # Hawaii
+    "District of Hawaii",
+    # Idaho
+    "District of Idaho",
+    # Illinois
+    "Central District of Illinois",
+    "Northern District of Illinois",
+    "Southern District of Illinois",
+    # Indiana
+    "Northern District of Indiana",
+    "Southern District of Indiana",
+    # Iowa
+    "Northern District of Iowa",
+    "Southern District of Iowa",
+    # Kansas
+    "District of Kansas",
+    # Kentucky
+    "Eastern District of Kentucky",
+    "Western District of Kentucky",
+    # Louisiana
+    "Eastern District of Louisiana",
+    "Middle District of Louisiana",
+    "Western District of Louisiana",
+    # Maine
+    "District of Maine",
+    # Maryland
+    "District of Maryland",
+    # Massachusetts
+    "District of Massachusetts",
+    # Michigan
+    "Eastern District of Michigan",
+    "Western District of Michigan",
+    # Minnesota
+    "District of Minnesota",
+    # Mississippi
+    "Northern District of Mississippi",
+    "Southern District of Mississippi",
+    # Missouri
+    "Eastern District of Missouri",
+    "Western District of Missouri",
+    # Montana
+    "District of Montana",
+    # Nebraska
+    "District of Nebraska",
+    # Nevada
+    "District of Nevada",
+    # New Hampshire
+    "District of New Hampshire",
+    # New Jersey
+    "District of New Jersey",
+    # New Mexico
+    "District of New Mexico",
+    # New York
+    "Eastern District of New York",
+    "Northern District of New York",
+    "Southern District of New York",
+    "Western District of New York",
+    # North Carolina
+    "Eastern District of North Carolina",
+    "Middle District of North Carolina",
+    "Western District of North Carolina",
+    # North Dakota
+    "District of North Dakota",
+    # Ohio
+    "Northern District of Ohio",
+    "Southern District of Ohio",
+    # Oklahoma
+    "Eastern District of Oklahoma",
+    "Northern District of Oklahoma",
+    "Western District of Oklahoma",
+    # Oregon
+    "District of Oregon",
+    # Pennsylvania
+    "Eastern District of Pennsylvania",
+    "Middle District of Pennsylvania",
+    "Western District of Pennsylvania",
+    # Rhode Island
+    "District of Rhode Island",
+    # South Carolina
+    "District of South Carolina",
+    # South Dakota
+    "District of South Dakota",
+    # Tennessee
+    "Eastern District of Tennessee",
+    "Middle District of Tennessee",
+    "Western District of Tennessee",
+    # Texas
+    "Eastern District of Texas",
+    "Northern District of Texas",
+    "Southern District of Texas",
+    "Western District of Texas",
+    # Utah
+    "District of Utah",
+    # Vermont
+    "District of Vermont",
+    # Virginia
+    "Eastern District of Virginia",
+    "Western District of Virginia",
+    # Washington
+    "Eastern District of Washington",
+    "Western District of Washington",
+    # West Virginia
+    "Northern District of West Virginia",
+    "Southern District of West Virginia",
+    # Wisconsin
+    "Eastern District of Wisconsin",
+    "Western District of Wisconsin",
+    # Wyoming
+    "District of Wyoming",
+    # Territories
+    "District of Guam",
+    "District of the Northern Mariana Islands",
+    "District of Puerto Rico",
+    "District of the Virgin Islands",
+]
